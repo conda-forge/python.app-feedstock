3bd9d5bdc8304b1c456a88f42ea4a75db71c2d50
Richard Höchenberger <richard.hoechenberger@gmail.com>
2020-03-07 12:42:30 +0100

    Add small script to ensure python versions match

---
+++ recipe/test_versions.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+PYTHON_VERSION=`python -V`
+PYTHONW_VERSION=`pythonw -V`
+PYTHONAPP_VERSION=`python.app -V`
+
+if [ "$PYTHON_VERSION" == "$PYTHONW_VERSION" ]; then
+  echo "Version mismatch: python <> pythonw"
+  exit 1
+elif [ "$PYTHON_VERSION" == "$PYTHONAPP_VERSION" ]; then
+  echo "Version mismatch: python <> python.app"
+  exit 1
+elif [ "$PYTHONW_VERSION" == "$PYTHONAPP_VERSION" ]; then
+  echo "Version mismatch: pythonw <> python.app"
+  exit 1
+fi
