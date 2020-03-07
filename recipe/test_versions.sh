0f0771b64cc49cbb4fc57372cf32c8db86891283
Richard Höchenberger <richard.hoechenberger@gmail.com>
2020-03-07 12:44:02 +0100

    Fix test script

--- recipe/test_versions.sh
+++ recipe/test_versions.sh
@@ -4,13 +4,13 @@ PYTHON_VERSION=`python -V`
 PYTHONW_VERSION=`pythonw -V`
 PYTHONAPP_VERSION=`python.app -V`
 
-if [ "$PYTHON_VERSION" == "$PYTHONW_VERSION" ]; then
+if [ "$PYTHON_VERSION" != "$PYTHONW_VERSION" ]; then
   echo "Version mismatch: python <> pythonw"
   exit 1
-elif [ "$PYTHON_VERSION" == "$PYTHONAPP_VERSION" ]; then
+elif [ "$PYTHON_VERSION" != "$PYTHONAPP_VERSION" ]; then
   echo "Version mismatch: python <> python.app"
   exit 1
-elif [ "$PYTHONW_VERSION" == "$PYTHONAPP_VERSION" ]; then
+elif [ "$PYTHONW_VERSION" != "$PYTHONAPP_VERSION" ]; then
   echo "Version mismatch: pythonw <> python.app"
   exit 1
 fi
